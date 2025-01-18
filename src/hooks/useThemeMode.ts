42c87d1a2e4f61c18398806060b3a0fb6098ae2c
mustafakumshi <kumshimustafa0@gmail.com>
2025-01-18 13:25:37 +0530

    Add Header Component

--- src/hooks/useThemeMode.ts
+++ src/hooks/useThemeMode.ts
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../store';
 import { toggleThemeMode } from '../store/themeSlice';
+import { useEffect } from 'react';
 
 const useThemeMode = () => {
   const dispatch = useDispatch();
@@ -9,6 +10,16 @@ const useThemeMode = () => {
   const toggleMode = () => {
     dispatch(toggleThemeMode());
   };
+
+  // Update the data-theme attribute in body when mode changes
+  useEffect(() => {
+    // Set the data-theme attribute on body
+    if (mode === 'dark') {
+      document.body.setAttribute('data-theme', 'dark');
+    } else {
+      document.body.setAttribute('data-theme', 'light');
+    }
+  }, [mode]); // Run this effect whenever the mode changes
 
   return { mode, toggleMode };
 };
